3e20caefff6570279d6954ebe107f3a4661f5b54
obestwalter <oliver@bestwalter.de>
2013-04-28 07:45:06 +0200

    formatting

--- loslassa/loslassa.py
+++ loslassa/loslassa.py
@@ -12,7 +12,8 @@ Other important ingredients:
     * `sphinx <http://sphinx-doc.org>`_  the documentation generator
     * simple reloading development server based on code from
       `Werkzeug  <http://www.pocoo.org/projects/werkzeug/#werkzeug>`_
-    * `Plumbum <http://plumbum.readthedocs.org/en/latest/>`_ for convenient shell access
+    * `Plumbum <http://plumbum.readthedocs.org/en/latest/>`_
+      for convenient shell access
     * Permissive `BSD License <https://en.wikipedia.org/wiki/BSD_licenses>`_
 
 ============
@@ -42,9 +43,10 @@ work locally with simple text files, let some clever system (sphinx) generate
 the HTML and Javascript and then push the results online.
 
 The basic workflow is inspired by the way how developing for example a
-`flask <http://flask.pocoo.org/>`_ web application works: a local server runs in the
-background while you edit your files and it reloads the changes as soon as they
-happen. This makes it very easy to make quick changes and see the
+`flask <http://flask.pocoo.org/>`_ web application works: a local
+server runs in the background while you edit your files and it reloads
+the changes as soon as they happen.
+This makes it very easy to make quick changes and see the
 results right away. Any errors or problems are logged to the console or are
 shown right in the HTML output.
 
@@ -66,7 +68,8 @@ similar to sphinx-quickstart only simpler and tailored to only HTML output.
 Play
 ====
 Playing with the source and create your page. Add content and see the
-changes right away thanks to local server with automatic rebuild of the web pages::
+changes right away thanks to local server with automatic
+rebuild of the web pages::
 
     cd */path/to/project*
     loslassa play
@@ -83,7 +86,8 @@ This part is a bit vague still but basically it should simply push the
 generated pages to the server, by maintaining them in a git repository
 
 First time publishing would clone the repository bare to the web space and
-set it to be origin from then on ... or summin like that, didn't think that through yet::
+set it to be origin from then on
+... or summin like that, didn't think that through yet::
 
     cd */path/to/project*
     loslassa loslassa
@@ -99,8 +103,8 @@ do more involved stuff via sphinx extensions.
 About the name
 ==============
 
-**Loslassa** or **los lassa** means to let go and relax in
-a german dialect called `Swabian <http://en.wikipedia.org/wiki/Swabian_German>`_
+**Loslassa** or **los lassa** means to let go and relax in a german dialect
+called `Swabian <http://en.wikipedia.org/wiki/Swabian_German>`_
 spoken in parts of South Germany. As I moved into this part of Germany in
 2011 I came in direct contact with this dialect and I am still quite in
 awe of it, but I really like it ... or at least I am really trying very hard to
@@ -111,8 +115,9 @@ and my Swabian Yoga teacher always says "loslassa" whenever she wants us to
 relax after some contortion she made us go through - so this is my favorite
 part of the lessons.
 
-So in the true spirit of **Loslassa** I hope this little project helps you let go of your
-preconceptions how web pages have to be created and you try the Loslassa way ;).
+So in the true spirit of **Loslassa** I hope this little project helps
+you let go of your preconceptions how web pages have to be
+created and you try the Loslassa way ;).
 
 ===========
 Inspiration
