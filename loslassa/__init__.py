f08acf71bb72034eee8589e659bac009fb145d71
Oliver Bestwatler <oliver@bestwalter.de>
2013-04-30 13:23:28 +0300

    Update __init__.py
    typo

--- loslassa/__init__.py
+++ loslassa/__init__.py
@@ -1,4 +1,4 @@
-"""WARNING: Dont't do anything here - shadowed by loslassy.py.
+"""WARNING: Dont't do anything here - shadowed by loslassa.py.
 
 I know that this is a bit naughty, but I want the
 loslassa/loslassa/loslassa mantra to be part of the project structure ;)
