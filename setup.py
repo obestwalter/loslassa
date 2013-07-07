5b890e5da28e87ae2ca55859d617d930f40d975c
obestwalter <oliver@bestwalter.de>
2013-07-08 00:57:21 +0200

    what matters for the user?

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from loslassa.loslassa import __doc__, __version__
 setup(
     name='Loslassa',
     description=('A simple way to interactively develop web pages with '
-                 'Python, reStructuredText, git and love :)'),
+                 'reStructuredText and love :)'),
     version=__version__,
     license='BSD',
     url='http://github.com/obestwalter/loslassa/',
